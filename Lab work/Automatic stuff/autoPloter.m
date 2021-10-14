fb234dc0bbdb69fd81828d0615b698c50a1f0013
Henning Frestad <henning.frestad@gmail.com>
2021-10-14 14:48:55 +0200

    Fixed autostuff

--- Lab work/Automatic stuff/autoPloter.m
+++ Lab work/Automatic stuff/autoPloter.m
@@ -1,21 +1,11 @@
-function [figHandle] = autoPloter(file_struct,rows,endTime)
+function [figHandle] = autoPloter(file_struct,rows)
 %AUTOPLOTER Summary of this function goes here
 %   Detailed explanation goes here
 var = file_struct.var;
 time_row = var(1,:);
-if exist('endTime', 'var')
-    %edit stop time
-    endTimeNr=find( time_row >= endTime, 1 );
-    time_row=time_row(1, 1:endTimeNr);
-    var_To_Plot = var(rows,1:endTimeNr);
-else
-    var_To_Plot = var(rows,:);
-end
 
+var_To_Plot = var(rows,:);
 plot(time_row,var_To_Plot);
 figHandle = gca;
 grid
-endTime=time_row(length(time_row));
-figHandle.XLim=[0,endTime];
-
 end
